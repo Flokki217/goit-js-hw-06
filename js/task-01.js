b4da13952be87ad1a0355ccd0639bbe4d59df4a8
Flokki217 <flokki217@gmail.com>
2023-06-30 15:47:16 +0200

    123

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,9 @@
-const listEl = document.querySelectorAll(".item");
-console.log(listEl);
-
-// const firstListItem = list.sexondElementChild;
-// console.log(firstListItem);
+const catElemItems = document.querySelectorAll("#categories > li.item");
+const itemsCount = catElemItems.length;
+console.log(`Number of categories: ${itemsCount} `);
+catElemItems.forEach(function (category) {
+  const catElName = category.querySelector("h2").textContent;
+  const categoryElements = category.querySelectorAll("li").length;
+  console.log(`Category: ${catElName}`);
+  console.log(`Elements: ${categoryElements}`);
+});
