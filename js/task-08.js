aaa2e11c2e8840606b938949ad1eb1d9865982a2
Flokki217 <flokki217@gmail.com>
2023-07-01 18:02:09 +0200

    qwerty

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", checkSubmit);
 
+function checkSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return window.alert("Please, field can't be empty!");
+  }
+  console.log(`User information = {
+        Email: ${email.value}   Password: ${password.value}
+    }`);
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyEl = document.querySelector("body");
+const colorTextEl = document.querySelector(".color");
+const buttonEl = document.querySelector(".change-color");
+buttonEl.addEventListener("click", changeColor);
+
+function changeColor(event) {
+  const theColorNew = getRandomHexColor();
+  colorTextEl.textContent = theColorNew;
+  bodyEl.style.backgroundColor = theColorNew;
+}
